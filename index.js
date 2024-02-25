2f18a141630632e85aa4dedf6daac556df069d74
luke <luke@deb.res6.spectrum.com>
2024-02-25 13:04:53 -0500

    added PUT and GET requsts for jsonBlob

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 $(function() {
   var offset = 0;
   var rpp = 9;
-  let selectedId = 0;
-  let contentCleared = false;
+  let selectedIndex = 0;
 
-  function getIndex(id) {
-    for (let i = 0; i < pets.length; i++) {
-      if (pets[i].id == id) {
+  let apiUrl = "https://jsonblob.com/api/jsonBlob/1210772481471537152";
+
+  function getIndex(id, arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].id == id) {
         return i;
       }
     }
@@ -26,18 +27,18 @@ $(function() {
     return count === 0;
   }
 
-  function createInnerCard(i) {
+  function createInnerCard(index, pets) {
     return `
-    <h1 id="name-${pets[i].id}">${pets[i].name}</h1>
-    <img id="image-${pets[i].id}" src="./images/pets/${pets[i].id}.jpg">
-    <p><strong>Age: </strong><span id="age-${pets[i].id}">${pets[i].age}</span></p>
-    <p><strong>Sex: </strong><span id="sex-${pets[i].id}">${pets[i].sex}</span></p>
-    <p><strong>Type: </strong><span id="type-${pets[i].id}">${pets[i].type}</span></p>
-    <a href="detail.html?item=${pets[i].id}" id="link-${pets[i].id}"></a>
-    <div class="d-flex justify-content-center" id="btn-box-${pets[i].id}">
-        <button id="update-${pets[i].id}" data-id="${pets[i].id}" class="btn btn-secondary update-btn" data-bs-toggle="modal" data-bs-target="#modal-update">Update</button>
-        <button id="delete-${pets[i].id}" class="btn btn-sm btn-danger btn-delete">Delete</button>
-        <button class="btn btn-primary getButton load-btn" type="button" data-bs-toggle="modal" data-bs-target="#modal" data-id="${pets[i].id}">See Details</button>
+    <h1 id="name-${pets[index].id}">${pets[index].name}</h1>
+    <img id="image-${pets[index].id}" src="./images/pets/${pets[index].id}.jpg">
+    <p><strong>Age: </strong><span id="age-${pets[index].id}">${pets[index].age}</span></p>
+    <p><strong>Sex: </strong><span id="sex-${pets[index].id}">${pets[index].sex}</span></p>
+    <p><strong>Type: </strong><span id="type-${pets[index].id}">${pets[index].type}</span></p>
+    <a href="detail.html?item=${pets[index].id}" id="link-${pets[index].id}"></a>
+    <div class="d-flex justify-content-center" id="btn-box-${pets[index].id}">
+        <button id="update-${pets[index].id}" data-id="${pets[index].id}" class="btn btn-secondary update-btn" data-bs-toggle="modal" data-bs-target="#modal-update">Update</button>
+        <button id="delete-${pets[index].id}" class="btn btn-sm btn-danger btn-delete">Delete</button>
+        <button class="btn btn-primary getButton load-btn" type="button" data-bs-toggle="modal" data-bs-target="#modal" data-id="${pets[index].id}">See Details</button>
     </div>
   `;
   }
@@ -52,72 +53,79 @@ $(function() {
   });
   document.querySelector("#loadmore").append(loadMoreButton);
 
-  function createCard(i) {
+  function createCard(index, pets) {
     let card = document.createElement("div");
     card.classList.add("card");
-    card.id = `card-${pets[i].id}`;
-    card.setAttribute("data-id", pets[i].id);
-    card.innerHTML = createInnerCard(i);
+    card.id = `card-${pets[index].id}`;
+    card.setAttribute("data-id", pets[index].id);
+    card.innerHTML = createInnerCard(index, pets);
     document.getElementById("content").append(card);
     if (offset + rpp > pets.length) {
       loadMoreButton.hidden = true;
     }
   }
 
-  function displayElements() {
-    for (
-      let i = contentCleared ? 0 : offset;
-      i < Math.min(offset + rpp, pets.length);
-      i++
-    ) {
-      if (pets[i]) {
-        createCard(i);
+  function displayElements(contentCleared = false) {
+    $.get(apiUrl, function(data) {
+      pets = data;
+      for (
+        let i = contentCleared ? 0 : offset;
+        i < Math.min(offset + rpp, pets.length);
+        i++
+      ) {
+        createCard(i, pets);
       }
-    }
+    });
   }
 
   displayElements();
 
   $(document).on("click", ".btn-danger", function() {
     let cardId = getIndex(parseInt($(this).closest(".card").attr("data-id")));
-    console.log(cardId);
     pets.splice(cardId, 1);
     $(this).closest(".card").remove();
-
     $("#content").empty();
-    contentCleared = true;
-    displayElements();
-    contentCleared = false;
+    displayElements(true);
   });
 
   $(document).on("click", ".getButton", function() {
     let animalId = parseInt($(this).attr("data-id"));
-    let id = getIndex(animalId);
-    $("#modal [name=name]").text(pets[id].name);
-    $("#modal [name=breed]").text(pets[id].breed);
-    $("#sex").text(pets[id].sex);
-    $("#age").text(pets[id].age);
-    $("#color").text(pets[id].color);
-    $("#fixed").text(pets[id].fixed);
-    $("#id").text(pets[id].id);
-    $("#microchip_num").text(pets[id].microchip_num);
-    $("#modal a").attr("href", "detail.html?item=" + animalId);
+    $.get(apiUrl, function(data) {
+      let pets = data;
+      let id = getIndex(animalId, pets);
+      $("#modal [name=name]").text(pets[id].name);
+      $("#modal [name=breed]").text(pets[id].breed);
+      $("#sex").text(pets[id].sex);
+      $("#age").text(pets[id].age);
+      $("#color").text(pets[id].color);
+      $("#fixed").text(pets[id].fixed);
+      $("#id").text(pets[id].id);
+      $("#microchip_num").text(pets[id].microchip_num);
+      $("#modal a").attr("href", "detail.html?item=" + animalId);
+    });
   });
 
   $(document).on("click", ".update-btn", function() {
-    let update_id = getIndex(parseInt($(this).attr("data-id")));
-    selectedId = update_id;
-    $('#update-area [name="name"]').val(pets[update_id].name);
-    $('#update-area [name="age"]').val(pets[update_id].age);
-    $('#update-area [name="sex"]').val(pets[update_id].sex);
-    $('#update-area [name="breed"]').val(pets[update_id].breed);
-    $('#update-area [name="color"]').val(pets[update_id].color);
-    $('#update-area [name="fixed"]').prop("checked", pets[update_id].fixed);
-    $('#update-area [name="microchip_num"]').val(pets[update_id].microchip_num);
-    $('#update-area [name="summary"]').val(pets[update_id].summary);
-    $('#update-area [name="health_info"]').val(pets[update_id].health_info);
-    $('#update-area [name="type"]').val(pets[update_id].type);
-    $('#update-area [name="image"]').val(pets[update_id].image);
+    let petId = parseInt($(this).attr("data-id"));
+    $.get(apiUrl, function(data) {
+      let pets = data;
+      let petIndex = getIndex(petId, pets);
+      selectedIndex = petIndex;
+
+      $('#update-area [name="name"]').val(pets[petIndex].name);
+      $('#update-area [name="age"]').val(pets[petIndex].age);
+      $('#update-area [name="sex"]').val(pets[petIndex].sex);
+      $('#update-area [name="breed"]').val(pets[petIndex].breed);
+      $('#update-area [name="color"]').val(pets[petIndex].color);
+      $('#update-area [name="fixed"]').prop("checked", pets[petIndex].fixed);
+      $('#update-area [name="microchip_num"]').val(
+        pets[petIndex].microchip_num,
+      );
+      $('#update-area [name="summary"]').val(pets[petIndex].summary);
+      $('#update-area [name="health_info"]').val(pets[petIndex].health_info);
+      $('#update-area [name="type"]').val(pets[petIndex].type);
+      $('#update-area [name="image"]').val(pets[petIndex].image);
+    });
   });
 
   $(document).on("click", "#btn-update", function() {
@@ -133,62 +141,66 @@ $(function() {
     var type = $('#update-area [name="type"]').val();
     var image = $('#update-area [name="image"]').val();
 
-    pets[selectedId].name = name;
-    pets[selectedId].age = age;
-    pets[selectedId].sex = sex;
-    pets[selectedId].breed = breed;
-    pets[selectedId].color = color;
-    pets[selectedId].fixed = fixed;
-    pets[selectedId].microchip_num = microchip_num;
-    pets[selectedId].summary = summary;
-    pets[selectedId].health_info = health_info;
-    pets[selectedId].type = type;
-    pets[selectedId].image = image;
+    pets[selectedIndex].name = name;
+    pets[selectedIndex].age = age;
+    pets[selectedIndex].sex = sex;
+    pets[selectedIndex].breed = breed;
+    pets[selectedIndex].color = color;
+    pets[selectedIndex].fixed = fixed;
+    pets[selectedIndex].microchip_num = microchip_num;
+    pets[selectedIndex].summary = summary;
+    pets[selectedIndex].health_info = health_info;
+    pets[selectedIndex].type = type;
+    pets[selectedIndex].image = image;
 
-    $(`#name-${pets[selectedId].id}`).text(name);
-    $(`#image-${pets[selectedId].id}`).text(image);
-    $(`#age-${pets[selectedId].id}`).text(age);
-    $(`#sex-${pets[selectedId].id}`).text(sex);
-    $(`#type-${pets[selectedId].id}`).text(type);
+    $(`#name-${pets[selectedIndex].id}`).text(name);
+    $(`#image-${pets[selectedIndex].id}`).text(image);
+    $(`#age-${pets[selectedIndex].id}`).text(age);
+    $(`#sex-${pets[selectedIndex].id}`).text(sex);
+    $(`#type-${pets[selectedIndex].id}`).text(type);
   });
 
   // "Create" button (adding input to array)
 
   $(document).on("click", "#btn-save", function() {
     let randomId = Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
-    var name = $('#input-area [name="name"]').val();
-    var age = $('#input-area [name="age"]').val();
-    var sex = $('#input-area [name="sex"]').val();
-    var breed = $('#input-area [name="breed"]').val();
-    var color = $('#input-area [name="color"]').val();
-    var fixed = $('#input-area [name="fixed"]').is(":checked");
-    var microchip_num = $('#input-area [name="microchip_num"]').val();
-    var summary = $('#input-area [name="summary"]').val();
-    var health_info = $('#input-area [name="health_info"]').val();
-    var type = $('#input-area [name="type"]').val();
-    var image = $('#input-area [name="image"]').val();
-
     if (!isUniqueId(randomId)) {
       alert("error generating unique id. Try again.");
+      return;
     }
 
     var newPet = {
-      name: name,
-      age: age,
-      sex: sex,
-      breed: breed,
-      color: color,
-      fixed: fixed,
       id: randomId,
-      microchip_num: microchip_num,
-      summary: summary,
-      health_info: health_info,
-      type: type,
-      image: image,
+      name: $('#input-area [name="name"]').val(),
+      age: $('#input-area [name="age"]').val(),
+      sex: $('#input-area [name="sex"]').val(),
+      breed: $('#input-area [name="breed"]').val(),
+      color: $('#input-area [name="color"]').val(),
+      fixed: $('#input-area [name="fixed"]').is(":checked"),
+      microchip_num: $('#input-area [name="microchip_num"]').val(),
+      summary: $('#input-area [name="summary"]').val(),
+      health_info: $('#input-area [name="health_info"]').val(),
+      type: $('#input-area [name="type"]').val(),
+      image: $('#input-area [name="image"]').val(),
     };
-    pets.push(newPet);
-    if (pets.length - (offset + rpp) < 9) {
-      createCard(pets.length - 1);
-    }
+
+    $.get(apiUrl, function(data) {
+      let pets = data;
+      pets.push(newPet);
+      $.ajax({
+        type: "PUT",
+        url: apiUrl,
+        data: JSON.stringify(pets),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function() {
+          $("#content").empty();
+          displayElements(true);
+        },
+        error: function(errMsg) {
+          alert(errMsg);
+        },
+      });
+    });
   });
 });
